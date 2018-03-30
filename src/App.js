6aaa74c32e6ac8031967f54a69f4164cbaac22c1
Vincent Audoire <vincent@Vincents-MBP.fritz.box>
2018-03-30 20:14:59 +0200

    Set content

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="content">
+          <h1>Hi, I'm Vincent</h1>
+          <p>iOS developer <a href="https://next.n26.com">@N26</a></p>
+        </div>
+        <hr />
+        <div className="d-flex flex-row justify-content-center">
+          <a href="https://www.linkedin.com/in/vaudoire" className="p-2">Linkedin</a>
+          <a href="https://github.com/vincentaudoire" className="p-2">GitHub</a>
+          <a href="https://twitter.com/vincz_a" className="p-2">Twitter</a>
+        </div>
       </div>
     );
   }
